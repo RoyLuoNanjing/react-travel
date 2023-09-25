d7e7e4bdd6b2176dcb12d7f330403eda9ba15275
RoyLuoNanjing <115074266+RoyLuoNanjing@users.noreply.github.com>
2023-09-25 22:47:59 +0100

    My first typescript project

---
+++ README.txt
@@ -0,0 +1,2 @@
+项目亮点：
+css in JS (JSS) 即对css进行模块化
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import styles from './App.module.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className={styles['App-logo']} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles['App-link']}
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
