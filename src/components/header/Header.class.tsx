74a7647979b8279353eef8d8a0e4efa28dd61356
RoyLuoNanjing <115074266+RoyLuoNanjing@users.noreply.github.com>
2023-10-10 16:43:24 +0100

    [Update] New things with Redux

--- src/components/header/Header.class.tsx
+++ src/components/header/Header.class.tsx
@@ -1,72 +1,126 @@
 import React from "react";
-import logo from "../../assets/logo.svg";
 import styles from "./Header.module.css";
+import logo from "../../assets/logo.svg";
 import { Layout, Typography, Input, Menu, Button, Dropdown } from "antd";
 import { GlobalOutlined } from "@ant-design/icons";
 import { useParams, useLocation, useNavigate } from "react-router-dom";
-import { LanguageState } from "../../redux/languageReducer";
+import { withRouter, RouteComponentProps } from "../../helpers/withRouter";
+import store from "../../redux/store";
+import { LanguageState } from "../../redux/language/languageReducer";
+import { withTranslation, WithTranslation } from "react-i18next";
+import {
+  addLanguageActionCreator,
+  changeLanguageActionCreator,
+} from "../../redux/language/languageActions";
 
-export const Header: React.FC = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const params = useParams();
+interface State extends LanguageState {}
 
-  return (
-    <div className={styles["app-header"]}>
-      {/* top-header */}
-      <div className={styles["top-header"]}>
-        <div className={styles.inner}>
-          <Typography.Text className={styles["title-header"]}>
-            Make travel happier
-          </Typography.Text>
-          <Dropdown.Button
-            style={{ marginLeft: 15 }}
-            overlay={
-              <Menu
-                items={[
-                  { key: "1", label: "中文" },
-                  { key: "2", label: "English" },
-                ]}
-              />
-            }
-            icon={<GlobalOutlined />}
-          >
-            Languages
-          </Dropdown.Button>
-          <Button.Group className={styles["button-group"]}>
-            <Button onClick={() => navigate("/register")}>注册</Button>
-            <Button onClick={() => navigate("/signin")}>登录</Button>
-          </Button.Group>
+class HeaderComponent extends React.Component<
+  RouteComponentProps & WithTranslation,
+  State
+> {
+  constructor(props) {
+    super(props);
+    const storeState = store.getState();
+    this.state = {
+      language: storeState.language,
+      languageList: storeState.languageList,
+    };
+    store.subscribe(this.handleStoreChange);
+  }
+
+  handleStoreChange = () => {
+    const storeState = store.getState();
+    this.setState({
+      language: storeState.language,
+      languageList: storeState.languageList,
+    });
+  };
+
+  menuClickHandler = (e) => {
+    console.log(e);
+    if (e.key === "new") {
+      // 处理新语言添加action
+      const action = addLanguageActionCreator("新语言", "new_lang");
+      store.dispatch(action);
+    } else {
+      const action = changeLanguageActionCreator(e.key);
+      store.dispatch(action);
+    }
+  };
+
+  render(): React.ReactNode {
+    const { navigate, t } = this.props;
+    return (
+      <div className={styles["app-header"]}>
+        {/* top-header */}
+        <div className={styles["top-header"]}>
+          <div className={styles.inner}>
+            <Typography.Text>{t("header.slogan")}</Typography.Text>
+            <Dropdown.Button
+              style={{ marginLeft: 15 }}
+              overlay={
+                <Menu
+                  onClick={this.menuClickHandler}
+                  items={[
+                    ...this.state.languageList.map((l) => {
+                      return { key: l.code, label: l.name };
+                    }),
+                    { key: "new", label: t("header.add_new_language") },
+                  ]}
+                />
+              }
+              icon={<GlobalOutlined />}
+            >
+              {this.state.language === "zh" ? "中文" : "English"}
+            </Dropdown.Button>
+            <Button.Group className={styles["button-group"]}>
+              <Button onClick={() => navigate("/register")}>
+                {t("header.register")}
+              </Button>
+              <Button onClick={() => navigate("/signin")}>
+                {t("header.signin")}
+              </Button>
+            </Button.Group>
+          </div>
         </div>
-      </div>
-      <Layout.Header className={styles["main-header"]}>
-        <span onClick={() => navigate("/")}>
-          <img src={logo} alt="" className={styles["App-logo"]} />
-          <Typography.Title className={styles["title"]}>
-            Roy Travel Agency
-          </Typography.Title>
-        </span>
-        <Input.Search
-          placeholder={"please type in the destination, theme or keywords"}
-          className={styles["search-input"]}
+        <Layout.Header className={styles["main-header"]}>
+          <span onClick={() => navigate("/")}>
+            <img src={logo} alt="logo" className={styles["App-logo"]} />
+            <Typography.Title level={3} className={styles.title}>
+              {t("header.title")}
+            </Typography.Title>
+          </span>
+          <Input.Search
+            placeholder={"请输入旅游目的地、主题、或关键字"}
+            className={styles["search-input"]}
+          />
+        </Layout.Header>
+        <Menu
+          mode={"horizontal"}
+          className={styles["main-menu"]}
+          items={[
+            { key: "1", label: t("header.home_page") },
+            { key: "2", label: t("header.weekend") },
+            { key: "3", label: t("header.group") },
+            { key: "4", label: t("header.backpack") },
+            { key: "5", label: t("header.private") },
+            { key: "6", label: t("header.cruise") },
+            { key: "7", label: t("header.hotel") },
+            { key: "8", label: t("header.local") },
+            { key: "9", label: t("header.theme") },
+            { key: "10", label: t("header.custom") },
+            { key: "11", label: t("header.study") },
+            { key: "12", label: t("header.visa") },
+            { key: "13", label: t("header.enterprise") },
+            { key: "14", label: t("header.high_end") },
+            { key: "15", label: t("header.outdoor") },
+            { key: "16", label: t("header.insurance") },
+          ]}
         />
-      </Layout.Header>
-      <Menu
-        mode={"horizontal"}
-        className={styles["main-menu"]}
-        items={[
-          { key: 1, label: "Main" },
-          { key: 2, label: "Flight" },
-          { key: 3, label: "Hotel" },
-          { key: 4, label: "Flight + Hotel" },
-          { key: 5, label: "Car Rentals" },
-          { key: 6, label: "Cruise" },
-          { key: 7, label: "Attractions" },
-          { key: 8, label: "Aiport Taxis" },
-          { key: 9, label: "Special" },
-          { key: 10, label: "Sales" },
-        ]}
-      ></Menu>
-    </div>
-  );
-};
+      </div>
+    );
+  }
+}
+
+export const Header = withTranslation()(withRouter(HeaderComponent));
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,5 @@
-import { createStore } from "redux";
-import languageReducer from "./languageReducer";
+import { createStore } from 'redux';
+import languageReducer from "./language/languageReducer";
 
 const store = createStore(languageReducer);
 
