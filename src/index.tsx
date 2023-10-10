48af5a745f0a548168be200c066e81ab305f1d35
RoyLuoNanjing <115074266+RoyLuoNanjing@users.noreply.github.com>
2023-10-10 19:00:41 +0100

    [Feature] Add ajax

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,10 @@ import "antd/dist/reset.css"; //全局引用 ant design
 import "./i18n/configs";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import axios from "axios";
+
+//调用api时候不用每次都声明x-icode
+axios.defaults.headers["x-icode"] = "5D1DFB0DABDA9144";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
