7e7d24aa7f4c703ebc4aaf8efe4e6bfd6fe10391
RoyLuoNanjing <115074266+RoyLuoNanjing@users.noreply.github.com>
2023-10-16 17:46:17 +0100

    【Updat] new

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Spin, Row, Col, DatePicker, Space } from "antd";
+import styles from "./DetailPage.module.css";
+import { Header, Footer } from "../../components";
+
+const { RangePicker } = DatePicker;
 
 type MatchParams = {
   touristRouteId: string;
@@ -7,12 +13,70 @@ type MatchParams = {
 };
 
 export const DetailPage: React.FC = () => {
-  var params = useParams<MatchParams>();
+  const { touristRouteId } = useParams<MatchParams>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [product, setProduct] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const { data } = await axios.get(
+          `http://82.157.43.234:8080/api/touristRoutes/${touristRouteId}`
+        );
+        setProduct(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error instanceof Error ? error.message : "error");
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <Spin
+        size="large"
+        style={{
+          marginTop: 200,
+          marginBottom: 200,
+          marginLeft: "auto",
+          marginRight: "auto",
+          width: "100%",
+        }}
+      />
+    );
+  }
+  if (error) {
+    return <div>网站出错：{error}</div>;
+  }
   return (
-    <h1>
-      旅游路线详情页,路线id:{params.touristRouteId}
-      {params.other}
-    </h1>
+    <>
+      <Header />
+      <div className={styles["page-content"]}>
+        {/* 产品简介 与 日期选择 */}
+        <div className={styles["product-intro-container"]}>
+          <Row>
+            <Col span={13}></Col>
+            <Col span={11}>
+              <RangePicker open style={{ marginTop: 20 }} />
+            </Col>
+          </Row>
+        </div>
+        {/* 锚点菜单 */}
+        <div className={styles["product-detail-anchor"]}></div>
+        {/* 产品特色 */}
+        <div className={styles["product-detail-container"]}></div>
+        {/* 费用 */}
+        <div id="fees" className={styles["product-detail-container"]}></div>
+        {/* 预订须知 */}
+        <div id="notes" className={styles["product-detail-container"]}></div>
+        {/* 商品评价 */}
+        <div id="comments" className={styles["product-detail-container"]}></div>
+      </div>
+      <Footer />
+    </>
   );
 };
