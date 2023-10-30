8d695cae363b995704aa9b148b8663fc009ca670
RoyLuoNanjing <115074266+RoyLuoNanjing@users.noreply.github.com>
2023-10-30 19:46:45 +0000

    [Update] One chapter finished

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,15 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import languageReducer from "./language/languageReducer";
 import recommendProductsReducer from "./recommendProducts/recommendProductsReducer";
 import thunk from "redux-thunk";
 import { actionLog } from "./middlewares/actionLog";
-
+import { productDetailSlice } from "./productDetail/slice";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 const rootReducer = combineReducers({
   //使用对象来进行数据传递
   language: languageReducer,
   recommendProducts: recommendProductsReducer,
+  productDetail: productDetailSlice.reducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk, actionLog));
