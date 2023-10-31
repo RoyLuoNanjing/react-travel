bd5d6334efe8ca8adfabc1601fdf0d0318dfdcf9
RoyLuoNanjing <115074266+RoyLuoNanjing@users.noreply.github.com>
2023-10-31 21:26:59 +0000

    [Update] token persistance

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,9 @@ import App from "./App";
 import "antd/dist/reset.css"; //全局引用 ant design
 import "./i18n/configs";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import rootStore from "./redux/store";
 import axios from "axios";
+import { PersistGate } from "redux-persist/integration/react";
 
 //调用api时候不用每次都声明x-icode
 axios.defaults.headers["x-icode"] = "5D1DFB0DABDA9144";
@@ -16,8 +17,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider store={rootStore.store}>
+      <PersistGate persistor={rootStore.persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -7,6 +7,15 @@ import { productDetailSlice } from "./productDetail/slice";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { productSearchSlice } from "./productSearch/slice";
 import { userSlice } from "./user/slice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+/* token持久化配置信息 */
+const persistConfig = {
+  key: "root",
+  storage,
+  whiteList: ["user"], //表面把下面redux中user部分全部保存起来
+};
+
 const rootReducer = combineReducers({
   //使用对象来进行数据传递
   language: languageReducer,
@@ -18,12 +27,16 @@ const rootReducer = combineReducers({
 
 //const store = createStore(rootReducer, applyMiddleware(thunk, actionLog));
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(actionLog),
   devTools: true,
 });
+const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-export default store;
+export default { store, persistor };
